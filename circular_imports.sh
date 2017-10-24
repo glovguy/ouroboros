3664fa7826317a1e41b49d812297ca7b4dbb83b1
Karl <karlsmith@bouzou.com>
2017-10-24 00:32:58 -0400

    Uses git ls-files instead of find in order to respect .gitignore, changes search to BFS, resolves issues with recursion termination, constructs pythonified path in python script rather than in bash script, creates explicit cease criteria

--- circular_imports.sh
+++ circular_imports.sh
@@ -1,6 +1,6 @@
 # echo '' > directional_data.txt
-echo "path;eachline;pythonifiedpath;importedmodule" > directional_data.txt
-git ls-files | grep '.py$' > file_names.txt
+echo "path;eachline;importedmodule" > directional_data.txt
+git ls-files --full-name | grep '.py$' > file_names.txt
 while IFS=';' read -r path || [[ -n "$path" ]]; do
     foundstrs=$(cat $path | grep import)
     while read -r eachline; do
@@ -10,8 +10,8 @@ while IFS=';' read -r path || [[ -n "$path" ]]; do
             else
                 importedmodule=$(echo `expr "$eachline" : 'import \(.*\)'`)
             fi
-            pythonifiedpath=$(echo `expr "$path" : '\./\(.*\)\.py'` | sed -e 's/\//\./g')
-            echo "$path\t$eachline\t$pythonifiedpath\t$importedmodule" >> directional_data.txt
+            # pythonifiedpath=$(echo `expr "$path" : '\(.*\)\.py'` | sed -e 's/\//\./g')
+            echo -e "$path;$eachline;$importedmodule" >> directional_data.txt
         fi
     done <<< "$foundstrs"
 done < "file_names.txt"
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import csv
+import re
+
+def pythonify_path(path):
+    p = re.sub(r'.*(/).*', '.', path)
+    return re.sub(r'\.py', '', p)
 
 links_hash = {}
 firstLine = True
@@ -7,38 +12,46 @@ with open('directional_data.txt', 'r') as tsv:
         if firstLine:
             firstLine = False
             continue
-        if len(line) != 4:
+        if len(line) != 3:
             print("ERROR: ")
             print(line)
             continue
-        path, eachline, pythonifiedpath, importedmodule = line
+        path, eachline, importedmodule = line
+        pythonifiedpath = pythonify_path(path)
         if links_hash.get(pythonifiedpath) is None: links_hash[pythonifiedpath] = set()
         links_hash[pythonifiedpath].add(importedmodule)
 
 print(links_hash)
 print(len(links_hash))
 
-def DFS(node, visit, path):
-    for eachNode in links_hash[node]:
-        newPath = path + [node]
-        if links_hash.get(eachNode) is not None:
-            DFS(eachNode, visit, newPath)
-        else:
-            [visited_nodes.add(n) for n in path]
-    visit(node, path)
-
 global visited_nodes
 visited_nodes = set()
 
+def BFS(node, visit, path):
+    if visit(node, path): return
+    for eachNode in links_hash[node]:
+        newPath = path + [node]
+        if links_hash.get(eachNode) is not None:
+            BFS(eachNode, visit, newPath)
+        else:
+            [visited_nodes.add(n) for n in path]
+
+def _cease(node, path):
+    return node in path[:-1]
+
 def _loop_find(node, path):
-    if node in path[:-1]:
+    if _cease(node, path):
         print("Found a loop!")
+        print(node)
         print(path)
         [visited_nodes.add(n) for n in path]
+        return True
 
 def loop_find(node):
-    path = [node]
-    DFS(node, _loop_find, path)
+    path = []
+    BFS(node, _loop_find, path)
+
+
 
 print("\n\nBegin!")
 for eachNode in links_hash.keys():
