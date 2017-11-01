dc60d759839192aa89fc9b08059272cd8e670f54
Karl <karlsmith@bouzou.com>
2017-10-31 23:19:23 -0400

    Adds visualizations of graphs using networkx

---
+++ display/graph.py
@@ -0,0 +1,15 @@
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+from circularity import edges_from_loops
+
+
+def display_loops(loops):
+    allEdges = edges_from_loops(loops)
+    G = nx.DiGraph()
+    G.add_edges_from(allEdges)
+    pos = nx.spring_layout(G)
+    nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('Blues'), node_size = 500)
+    nx.draw_networkx_labels(G, pos)
+    nx.draw_networkx_edges(G, pos, edgelist=allEdges, edge_color='b', arrows=True)
+    plt.show()
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from nodeFactory import node_hash
 from circularity import find_loops, group_loops_by_module
+from display.graph import display_loops
 
-nodeHash = node_hash('directional_data.txt', ignore=[], verbose=True)
+nodeHash = node_hash('directional_data.txt', ignore=[], verbose=False)
+loops = find_loops(nodeHash, verbose=False)
+# problemModules = group_loops_by_module(loops, verbose=True)
 
-loops = find_loops(nodeHash, verbose=True)
+display_loops(loops)
 
-problemModules = group_loops_by_module(loops, verbose=True)
+
