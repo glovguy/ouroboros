43d87cb8f1175f6899f7b3922f18f4edca483ec9
Karl <karlsmith@bouzou.com>
2017-11-04 18:19:16 -0400

    Table is properly ordered from highest to lowest now

--- display/table.py
+++ display/table.py
@@ -4,5 +4,5 @@ from circularity import group_loops_by_module
 
 def display_problem_modules(loops):
     problemModules = group_loops_by_module(loops, verbose=False)
-    problemModuleStats = sorted([[mod, len(problemModules[mod])] for mod in problemModules], key=lambda pair: pair[1])
+    problemModuleStats = sorted([[mod, len(problemModules[mod])] for mod in problemModules], key=lambda pair: -pair[1])
     print tabulate(problemModuleStats, headers=['Module', '# of loops'])
